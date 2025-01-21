cfe204b894b8143fae7fe424bb6f60d36cffe121
Jon Heslop <jon@jonheslop.com>
2025-01-21 22:14:35 +0000

    bigger margin above footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export const Footer = () => (
-  <footer className="row-start-3 flex gap-4 lg:gap-12 xl:gap-8 flex-wrap items-center text-sm xl:grid grid-cols-3 2xl:grid-cols-8">
+  <footer className="row-start-3 flex gap-4 mt-16 lg:gap-12 xl:gap-8 flex-wrap items-center text-sm xl:grid grid-cols-3 2xl:grid-cols-8">
     <p className="text-balance 2xl:col-span-3">
       Thanks to <Link href="https://ethanmarcotte.com/">Ethan</Link> for{" "}
       <Link href="https://bukk.it/" className="underline">
