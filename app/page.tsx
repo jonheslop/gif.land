1242208e1c5ec0a3975a6810ae05daee6efd2895
Jon Heslop <jon@jonheslop.com>
2025-01-21 19:48:25 +0000

    Try it out

--- app/page.tsx
+++ app/page.tsx
@@ -1,100 +1,62 @@
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+import { turso, type fave } from "@/lib/turso";
+
+export default async function Home() {
+  const { rows } = await turso.execute("SELECT * FROM favourites");
+
+  console.log(rows);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+    <div className="flex flex-col min-h-screen p-8 gap-16 font-[family-name:var(--font-geist-sans)]">
+      <nav role="navigation">
+        <h1>
+          Jon’s{" "}
+          <Link href="https://bukk.it/" className="underline">
+            bukk.it
+          </Link>{" "}
+          faves
+        </h1>
+      </nav>
+      <main className="flex-1">
+        <ul className="grid md:grid-cols-2 lg:grid-cols-5 gap-6">
+          {rows.map((row) => (
+            <li key={row.id} className="flex flex-col gap-2">
+              <Image
+                src={`https://bukk.it/${row.url}`}
+                width={300}
+                height={300}
+                alt={row.url}
+              />
+              <p className="text-neutral-600">Tags: {row.tags}</p>
+            </li>
+          ))}
+        </ul>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer className="row-start-3 flex gap-4 flex-wrap items-center justify-center text-sm text-center">
+        <p>
+          Powered by{" "}
+          <Link href="https://turso.tech/" className="underline">
+            turso
+          </Link>
+          ,{" "}
+          <Link href="https://nextjs.org/" className="underline">
+            next.js
+          </Link>
+          , and{" "}
+          <Link href="https://tailwindcss.com/" className="underline">
+            tailwindcss
+          </Link>
+        </p>
+        <p className="text-balance">
+          Thanks to <Link href="https://ethanmarcotte.com/">Ethan</Link> for{" "}
+          <Link href="https://bukk.it/" className="underline">
+            bukk.it
+          </Link>
+          , the best archive of GIFs on the internet
+        </p>
+        <p>Built by Jon Heslop</p>
       </footer>
     </div>
   );
