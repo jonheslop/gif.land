158a80545f807ca84645a52fea4a01a13a967776
Jon Heslop <jon@jonheslop.com>
2025-01-21 21:22:53 +0000

    More grid breakpoints

--- components/grid.tsx
+++ components/grid.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from "react";
 
 export const Grid = ({ children }: { children: ReactNode }) => (
-  <ul className="grid md:grid-cols-2 lg:grid-cols-5 gap-8">{children}</ul>
+  <ul className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 2xl: grid-cols-8 gap-8">
+    {children}
+  </ul>
 );
