b10f41e554df588d115c5c11396c67969ba77bbb
Jon Heslop <jon@jonheslop.com>
2025-01-21 20:05:06 +0000

    Add Söhne font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,22 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const soehne = localFont({
+  src: [
+    {
+      path: "../fonts/soehne-web-buch.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../fonts/soehne-web-buch-kursiv.woff2",
+      weight: "400",
+      style: "italic",
+    },
+  ],
+  display: "swap",
+  variable: "--font-soehne",
 });
 
 export const metadata: Metadata = {
@@ -24,9 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${soehne.variable} font-soehne antialiased`}>
         {children}
       </body>
     </html>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        soehne: ["var(--font-soehne)"],
+      },
     },
   },
   plugins: [],
