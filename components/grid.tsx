03c371938adf79be654545cbbc33cc4874c668ba
Jon Heslop <jon@jonheslop.com>
2025-01-22 21:04:37 +0000

    Grid tweak

--- components/grid.tsx
+++ components/grid.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 export const Grid = ({ children }: { children: ReactNode }) => (
-  <ul className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-8 gap-8 lg:gap-4 xl:gap-8">
+  <ul className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-8 gap-8 lg:gap-x-4 2xl:gap-8 items-end">
     {children}
   </ul>
 );
