eeb9d3a20553a5ace2946996e51370afdfa6e731
Jon Heslop <jon@jonheslop.com>
2025-01-23 23:02:30 +0000

    Cache

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
+import { unstable_cache } from "next/cache";
 import { turso, type fave } from "@/lib/turso";
 import { Grid } from "@/components/grid";
 
+const getCachedResults = unstable_cache(
+  async () => {
+    const { rows } = await turso.execute(
+      "SELECT * FROM favourites ORDER BY url ASC",
+    );
+    return rows;
+  },
+  ["faves"],
+  {
+    tags: ["faves"],
+  },
+);
+
 export default async function Home() {
-  const { rows } = await turso.execute(
-    "SELECT * FROM favourites ORDER BY url ASC",
-  );
+  const rows = await getCachedResults();
   const faves = rows as fave[];
 
   return <Grid faves={faves} />;
