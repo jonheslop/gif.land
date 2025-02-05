3e0ff2022dcf13fd5de847e82b29c4d81f41d14e
Jon Heslop <jon@jonheslop.com>
2025-02-05 19:58:35 +0000

    Bigger images

--- components/grid.tsx
+++ components/grid.tsx
@@ -12,7 +12,7 @@ export const Grid = ({ faves }: { faves: fave[] }) => (
       </p>
     }
   >
-    <ul className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-8 gap-8 lg:gap-x-4 2xl:gap-8 items-end">
+    <ul className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-5 2xl:grid-cols-7 gap-8 lg:gap-x-4 2xl:gap-8 items-end">
       {faves.map((row) => (
         <Item key={row.id} item={row} />
       ))}
