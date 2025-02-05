b20e0c2e62f0b0fa48307bbfe6d7caa84ba83bd9
Jon Heslop <jon@jonheslop.com>
2025-02-05 17:30:59 +0000

    Make cmd+k focus search

--- components/search.tsx
+++ components/search.tsx
@@ -1,9 +1,25 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export const Search = () => {
   const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    const searchShortCutListener = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        document.getElementById("search")?.focus();
+        document.getElementById("search")?.classList.add("flash");
+      }
+    };
+
+    document.addEventListener("keydown", searchShortCutListener);
+
+    return () => {
+      document.removeEventListener("keydown", searchShortCutListener);
+    };
+  }, []);
 
   return (
     <form action={`/search/${searchTerm}`}>
@@ -17,6 +33,7 @@ export const Search = () => {
         onChange={(e) => setSearchTerm(e.target.value)}
         onBlur={(e) => {
           if (searchTerm !== "") e.target.form?.submit();
+          e.target.classList.remove("flash");
         }}
         className="border-b focus:border-neutral-900 ring-0 outline-none appearance-none dark:bg-neutral-950 rounded-none w-24 sm:w-auto"
       />
