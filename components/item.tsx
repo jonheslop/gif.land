bb656a350c225e1e4d2be74f3b387775f26861d9
Jon Heslop <jon@jonheslop.com>
2025-02-06 18:42:11 +0000

    Link to info page

--- components/item.tsx
+++ components/item.tsx
@@ -5,7 +5,7 @@ import { StyledLink as Link } from "./link";
 export const Item = ({ item }: { item: fave }) => {
   const tags = item.tags.split(", ");
   return (
-    <li className="flex flex-col gap-2">
+    <li className="flex flex-col gap-2 group">
       <Link target="_blank" href={`https://gif.land/${item.url}`}>
         <Image
           src={`https://gif.land/${item.url}`}
@@ -16,11 +16,20 @@ export const Item = ({ item }: { item: fave }) => {
         />
       </Link>
       <div>
-        <p className="text-sm leading-tight truncate" title={item.url}>
-          <Link target="_blank" href={`https://gif.land/${item.url}`}>
-            {item.url}
+        <header className="flex justify-between">
+          <p className="text-sm leading-tight truncate" title={item.url}>
+            <Link target="_blank" href={`https://gif.land/${item.url}`}>
+              {item.url}
+            </Link>
+          </p>
+          <Link
+            className="underline underline-offset-2 hover:text-emerald-700 hover:dark:text-emerald-500 text-sm leading-tight hidden group-hover:block"
+            target="_blank"
+            href={`/info/${item.url}`}
+          >
+            #
           </Link>
-        </p>
+        </header>
         <p className="text-sm leading-tight text-neutral-500 dark:text-neutral-400 mt-0.5">
           Tags:{" "}
           {tags.map((tag, index) => (
