8a9d46aafe54608d72471a48edc3d3e8f11ac8fc
Jon Heslop <jon@jonheslop.com>
2025-02-14 17:19:13 +0000

    revalidate

--- app/random/route.ts
+++ app/random/route.ts
@@ -1,6 +1,7 @@
 import { turso, type fave } from "@/lib/turso";
 
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export async function GET() {
   const { rows } = await turso.execute(
