47f11f17564dfce4cdb6f76c28a5da5aeb9c7af0
Jon Heslop <jon@jonheslop.com>
2025-02-19 17:33:11 +0000

    Footer stuff for full screen mode

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 import { StyledLink as Link } from "./link";
 
 export const Footer = () => (
-  <footer className="row-start-3 lg:mt-32 mt-16 flex-wrap items-start text-sm">
+  <footer className="row-start-3 lg:mt-32 mt-16 flex-wrap items-start text-sm relative z-50 mix-blend-plus-lighter hover:mix-blend-normal">
     <p className="xl:col-start-4 2xl:col-start-5 col-span-3">
       <Link
         className="underline hover:text-emerald-700 hover:dark:text-emerald-500"
