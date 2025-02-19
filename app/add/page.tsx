c9b47dc150d0b6d7f13ad5a7d9232beb0bc3a3c4
Jon Heslop <jon@jonheslop.com>
2025-02-19 22:57:03 +0000

    Hide add begin auth

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -1,6 +1,26 @@
-import { UploadForm } from "../../components/upload-form";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { UploadForm } from "@/components/upload-form";
+import { StyledLink as Link } from "@/components/link";
 
 export default async function AddNew() {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return (
+      <>
+        <h1 className="leading-tight text-3xl mb-6">Access denied</h1>
+        <Link href="/api/auth/signin">Sign in with GitHub</Link>
+      </>
+    );
+  }
+  if (session.user && session.user.email !== "jon@jonheslop.com") {
+    return (
+      <>
+        <h1 className="leading-tight text-3xl mb-6">Access still denied</h1>
+        <p className="text-neutral-500">Only Jon can upload files.</p>
+      </>
+    );
+  }
   return (
     <>
       <h1 className="leading-tight text-3xl">Upload a file</h1>
