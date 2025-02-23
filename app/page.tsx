1ad6a23c1cfde3c468b6c8e3ffd0a3846ca88d6a
Jon Heslop <jon@jonheslop.com>
2025-02-23 15:55:51 +0000

    Move into lib

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,9 @@
-import { unstable_cache } from "next/cache";
-import { turso, type fave } from "@/lib/turso";
+import { getPosts } from "@/lib/turso";
 import { Grid } from "@/components/grid";
 import { allFavesToPages } from "@/lib/helpers";
 
-const getPosts = unstable_cache(
-  async () => {
-    const { rows } = await turso.execute(
-      "SELECT * FROM favourites ORDER BY url ASC",
-    );
-    return rows as fave[];
-  },
-  ["getPosts"],
-  {
-    revalidate: 60,
-    tags: ["faves"],
-  },
-);
-
 export default async function Home() {
-  const faves = await getPosts();
+  const faves = await getPosts("SELECT * FROM favourites ORDER BY url ASC");
 
   const pagedFaves = allFavesToPages(faves);
 
--- lib/turso.ts
+++ lib/turso.ts
@@ -1,9 +1,23 @@
+import { unstable_cache } from "next/cache";
 import { createClient, Row } from "@libsql/client";
 
 export const turso = createClient({
   url: process.env.TURSO_DATABASE_URL || "",
   authToken: process.env.TURSO_AUTH_TOKEN,
 });
+
+// Check out how it’s working by duplicating DB rows and seeing if they show
+export const getPosts = (query: string) =>
+  unstable_cache(
+    async () => {
+      const { rows } = await turso.execute(query);
+      return rows as fave[];
+    },
+    [query],
+    {
+      tags: ["faves"],
+    },
+  )();
 
 export type fave = Row & {
   id: number;
